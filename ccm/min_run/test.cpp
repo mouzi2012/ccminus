0b7ca5e60150f58d1566c86f2b57f194f0f9609d
mouzi2012 <515258670@qq.com>
2017-06-11 21:04:18 +0800

    add test

---
+++ ccm/min_run/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "min_source.h"
+using namespace std;
+int main()
+{
+	MinSource source;
+	source.Init("haha");
+	std::cout<<"test"<<std::endl;
+	std::cout<<"hehe"<<std::endl;
+	return 0
+}
