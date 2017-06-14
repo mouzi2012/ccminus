5c6974def2e417d0399c42952d2409ca82432a14
mouzi2012 <515258670@qq.com>
2017-06-14 15:19:47 +0800

    add base parse code

--- ccm/min_run/parse/syntax/syntax.cpp
+++ ccm/min_run/parse/syntax/syntax.cpp
@@ -1,1 +1,125 @@
 #include "syntax.h"
+#include<iostream>
+using namespace std;
+
+
+
+
+MinToken::MinToken(MinSource* rhs) :pS(rhs)
+{
+
+}
+MinTokenRecord MinToken::getToken()
+{
+	char buff[256] = { 0 };
+	int pos = 0;
+	ECharType last_acc = ECharTypeEnd;
+	for (;;)
+	{
+		char c=0;
+		if (!pS)
+		{
+			cout << "null ps" << endl;
+			return;
+		}
+		if (pS->ReadC(c))
+		{
+			
+			if (c=='\r')
+			{
+				continue;
+			}
+			else if (IsLetter(c))
+			{
+				if (HandleStateChange(last_acc, ELetter))
+				{
+					buff[pos++] = c;
+					last_acc = ELetter;
+				}
+				else
+				{
+					cout << "handle letter error not need letter now" << endl;
+					MinTokenRecord r{ EErrorTrace, nullptr, 0 };
+					return r;
+				}
+			}
+			else if (IsDigit(c))
+			{
+				if (HandleStateChange(last_acc, EDigit))
+				{
+					buff[pos++] = c;
+					last_acc = EDigit;
+				}
+				else
+				{
+					cout << "handle EDigit error not need EDigit now" << endl;
+					MinTokenRecord r{ EErrorTrace, nullptr, 0 };
+					return r;
+				}
+			}
+			else if (IsWhiteSpace(c))
+			{
+
+			}
+
+		}
+		else
+		{
+
+		}
+		
+	}
+
+}
+bool MinToken::IsLetter(char c)
+{
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
+	{
+		return true;
+	}
+	return false;
+}
+bool MinToken::IsDigit(char c)
+{
+	if ('0' <= c && c <= '9')
+	{
+		return true;
+	}
+	return false;
+}
+bool MinToken::IsWhiteSpace(char c)
+{
+	if ((c == ' ') || (c == '\n') || (c == '\t'))
+	{
+		return true;
+	}
+	return false;
+}
+bool MinToken::HandleStateChange(ECharType o, ECharType n)
+{
+	switch (o)
+	{
+	case EDigit:
+	{
+		if (n == EDigit)
+		{
+			return true;
+		}
+		return false;
+	}
+	case ELetter:
+	{
+		if (n == ELetter)
+		{
+			return true;
+		}
+		return false;
+	}
+	case ECharTypeEnd:
+	{
+		return true;
+	}
+	default:
+		break;
+	}
+}
--- ccm/min_run/parse/syntax/syntax.h
+++ ccm/min_run/parse/syntax/syntax.h
@@ -1,12 +1,33 @@
 #ifndef MIN_SYNTAX_H
 #define MIN_SYNTAX_H
 #include "../../pool/string_pool.h"
-enum EMinTokenType{ENum,EID};
+#include "../../min_source.h"
+
+enum EMinTokenType{ENum,EID,EErrorTrace};
+enum ECharType{ EDigit, ELetter, ECharTypeEnd };
+
 struct MinTokenRecord
 {
 	EMinTokenType k;
 	StrNode* pn;	//the name if num the num string val
 	int	  i;		//simple just int num~
 };
-MinTokenRecord getToken();
+
+class MinToken
+{
+public:
+	MinToken(MinSource* rhs);
+public:
+	MinTokenRecord getToken();
+public:
+	static bool IsLetter(char c);
+	static bool IsDigit(char c);
+	static bool IsWhiteSpace(char c);
+	static bool HandleStateChange(ECharType o, ECharType n );
+private:
+	MinToken& operator=(MinToken& rhs);
+private:
+	MinSource* pS;
+};
+
 #endif
