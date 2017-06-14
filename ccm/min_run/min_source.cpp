063fea0436111875dfc25f8a69bd9488f6fc20bf
mouzi2012 <515258670@qq.com>
2017-06-14 14:17:16 +0800

    fix new error

--- ccm/min_run/min_source.cpp
+++ ccm/min_run/min_source.cpp
@@ -24,21 +24,21 @@ MinSource& MinSource::operator=(MinSource& s)
 	m_n = s.m_n;
 	if (m_n > 0)
 	{
-		m_i = new(std::nothrow) char(m_n);
+		m_i = new(std::nothrow) char[m_n];
 		if (m_i)
 		{
 			memcpy(m_i, s.m_i, m_n);
+			m_isLoad = s.m_isLoad;
 		}
-		
 	}
-	m_isLoad = s.m_isLoad;
+	
 	return *this;
 }
 MinSource::~MinSource()
 {
 	if (m_i)
 	{
-		delete m_i;
+		delete [] m_i;
 	}
 	memset(m_name, 0, sizeof(m_name));
 	m_p = 0;
